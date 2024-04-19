7783b2243a2412005e4d47ef93c8bb2d5b0c2ce0
OneTwoDan <danielondonoa@gmail.com>
2024-04-19 16:29:00 -0500

    Updated Navbar: In Progress

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
-      <h1>Bird Travel Agency</h1>
       <Home />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,23 +7,28 @@ const Header = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand href="#home">Bird Travel Agency</Navbar.Brand>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
+            <Nav.Link href="#destinations">Destinations</Nav.Link>
+            <NavDropdown title="Hotels" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">
+                Sofitel Barú Calablanca
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.2">
+                Hotel Las Américas Torre del Mar
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                Hotel York Luxury Suites
+              </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.4">
-                Separated link
+                Hyatt Regency Cartagena
               </NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link href="#flights">Flights</Nav.Link>
+            <Nav.Link href="#bookings">Bookings</Nav.Link>
+            <Nav.Link href="#login">Login</Nav.Link>
+            <Nav.Link href="#signup">Sign up</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
