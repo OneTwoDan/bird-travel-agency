5a53a3af591adf9fdcdf6d1ec362041ea66ca729
OneTwoDan <danielondonoa@gmail.com>
2024-04-19 16:02:18 -0500

    added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Button from "react-bootstrap/Button";
+
 function App() {
   return (
     <div className="App">
       <h1>Bird Travel Agency</h1>
+      <Button> Test </Button>
     </div>
   );
 }
