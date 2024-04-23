ad95f8283144532c1af72ba93b3ed1858155ff56
OneTwoDan <danielondonoa@gmail.com>
2024-04-22 20:31:37 -0500

    Navbar corrected

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,21 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import Logo from "../assets/logo.png";
+import "../styles/header.css";
 
 const Header = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">Bird Travel Agency</Navbar.Brand>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#destinations">Destinations</Nav.Link>
-            <NavDropdown title="Hotels" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">
-                Sofitel Barú Calablanca
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Hotel Las Américas Torre del Mar
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">
-                Hotel York Luxury Suites
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">
-                Hyatt Regency Cartagena
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#flights">Flights</Nav.Link>
-            <Nav.Link href="#bookings">Bookings</Nav.Link>
-            <Nav.Link href="#login">Login</Nav.Link>
-            <Nav.Link href="#signup">Sign up</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className="navbar-container">
+      <img className="logo-img" src={Logo} alt="Logo" />
+      <div className="navbar-elements">
+        <div className="navbar-element">Destinations</div>
+        <div className="navbar-element">Hotels</div>
+        <div className="navbar-element">Flights</div>
+        <div className="navbar-element">Bookings</div>
+        <div>
+          <button className="navbar-button">Login</button>
+          <button className="navbar-button">Sign up</button>
+        </div>
+      </div>
+    </div>
   );
 };
 
